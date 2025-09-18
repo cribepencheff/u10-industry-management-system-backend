e513a0a85dfc9a8e81e1821929ed5426dd2ce032
Kristoffer-L <kristoffer.larsson.02@gmail.com>
2025-09-18 16:15:33 +0200

    ft-graphql-inProgress-2

--- graphql/src/graphql/product/resolvers.js
+++ graphql/src/graphql/product/resolvers.js
@@ -1,12 +1,27 @@
 import ProductModel from "../../models/product.js";
 
 export const resolvers = {
-  products: async (_parent, args) => {
-    try {
-      const products = await ProductModel.find();
-      return products;
-    } catch (error) {
-      throw new Error("Error fetching products: " + error.message);
-    }
+  Query: {
+    products: async (_parent, args) => {
+      try {
+        const products = await ProductModel.find();
+        return products;
+      } catch (error) {
+        throw new Error("Error fetching products: " + error.message);
+      }
+    },
+    product: async (_parent, { id }) => {
+      try {
+        const products = await ProductModel.find(id);
+        return products;
+      } catch (error) {
+        throw new Error("Error fetching products: " + error.message);
+      }
+    },
+  },
+  Mutation: {
+    addProduct: async (_parent, { input }) => {
+      return ProductModel.create(input);
+    },
   },
 };
