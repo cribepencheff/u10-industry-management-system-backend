e44763b712e157bea6b775d236912e2460151c7d
Cribe Pencheff <cribe@self-made.se>
2025-09-18 10:39:01 +0200

    chore: Include .example.env + updaged manufacturer model

--- rest/src/config/db.js
+++ rest/src/config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  const CLUSTER_NAME = process.env.CLUSTER_NAME;
+  const DB_NAME = process.env.DB_NAME;
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(MONGODB_URI, { dbName: DB_NAME, appName: CLUSTER_NAME });
     console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
