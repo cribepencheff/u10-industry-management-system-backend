e39432a70ce48afdf63ce91af93d499dae9eb88f
Kristoffer-L <kristoffer.larsson.02@gmail.com>
2025-09-19 12:18:59 +0200

    kristoffer-branch-gql

---
+++ graphql/src/config/db.js
@@ -0,0 +1,24 @@
+import { success } from "../utils/colors.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const connectDB = async () => {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  const CLUSTER_NAME = process.env.CLUSTER_NAME;
+  const DB_NAME = process.env.DB_NAME;
+
+  if (!MONGODB_URI) throw new Error("MONGODB_URI not found in .env");
+  if (!DB_NAME) throw new Error("DB_NAME not found in .env");
+
+  await mongoose.connect(MONGODB_URI, {
+    dbName: DB_NAME,
+    appName: CLUSTER_NAME,
+  });
+
+  if (!mongoose.connection.db) {
+    throw new Error("MongoDB connection not initialized");
+  }
+  await mongoose.connection.db.admin().ping();
+  console.log(success("✓"), "MongoDB connected and responding");
+};
---
+++ graphql/src/utils/color.js
@@ -0,0 +1,4 @@
+export const success = (msg) => `\x1b[32m${msg}\x1b[0m`; // Green
+export const error = (msg) => `\x1b[31m${msg}\x1b[0m`; // Red
+export const info = (msg) => `\x1b[34m${msg}\x1b[0m`; // Blue
+export const warning = (msg) => `\x1b[33m${msg}\x1b[0m`; // Yellow
